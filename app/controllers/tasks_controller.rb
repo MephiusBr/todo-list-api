72e57a81e273b480d8dc1cd04466654039a3b31c
mephiusbr <matheusprofessionalbrazil@gmail.com>
2025-04-30 03:26:26 -0300

    add tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,22 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: :show
+
   def index
+    @tasks = Task.all
+    render json: @tasks.as_json(only: [:id, :name, :content])
   end
 
   def show
+    unless @task.present? then
+      return render json: { message: "task doesn't exist!" }, status: :bad_request
+    end
+
+    render json: @task.as_json(only: [:id,:name,:content])
+  end
+
+  private
+
+  def set_task
+    @task = Task.find_by(id: params[:id])
   end
 end
