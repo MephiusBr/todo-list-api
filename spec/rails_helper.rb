283289037d9b9cf9bece104e71459e41dfbf0a8f
mephiusbr <matheusprofessionalbrazil@gmail.com>
2025-04-30 03:24:33 -0300

    require helper methods and database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 
 require_relative './support/factory_bot'
+require_relative './support/helper_methods'
+require_relative './support/database_cleaner'
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 # Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
