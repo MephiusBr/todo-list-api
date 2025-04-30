b778e37f1ff64f5e5937dfc7b571012c10b64aa5
mephiusbr <matheusprofessionalbrazil@gmail.com>
2025-04-30 03:33:37 -0300

    refactor tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,20 +3,21 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
-    render json: @tasks.as_json(only: [:id, :name, :content])
+    render json: TaskPresenter.new(@tasks).call
   end
 
   def show
-    unless @task.present? then
-      return render json: { message: "task doesn't exist!" }, status: :bad_request
-    end
-
-    render json: @task.as_json(only: [:id,:name,:content])
+    return task_does_not_exist unless @task.present?
+    render json: TaskPresenter.new(@task).call
   end
 
   private
 
   def set_task
     @task = Task.find_by(id: params[:id])
+  end
+
+  def task_does_not_exist
+    render json: { message: "task doesn't exist!" }, status: :bad_request
   end
 end
