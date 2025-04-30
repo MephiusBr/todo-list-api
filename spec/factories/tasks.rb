07819f366c2af9df42eafcbd7fb0a56ef1fd4575
mephiusbr <matheusprofessionalbrazil@gmail.com>
2025-04-30 02:01:52 -0300

    add factory and spec for Task

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :task do
-    name { "MyString" }
-    content { "MyText" }
+    name { Faker::Company.industry }
+
+    trait :with_content do
+      content { Faker::Lorem.paragraph(sentence_count: 5) }
+    end
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "when it's valid" do
+      let(:task) { build(:task) }
+
+      it "has a unique name" do
+        expect(task).to be_valid
+      end
+    end
+
+    context "when it's invalid" do
+      let!(:persisted_task) { create(:task, name: "new task") }
+      let(:task) { build(:task, name: "new task") }
+
+      it "doesn't have a unique name" do
+        expect(task).to be_invalid
+      end
+    end
+  end
 end
