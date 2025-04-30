c89f14d46bb44c9098500613f7381100fb25e5ef
mephiusbr <matheusprofessionalbrazil@gmail.com>
2025-04-30 02:07:02 -0300

    update model spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe "validations" do
     context "when it's valid" do
-      let(:task) { build(:task) }
+      before { subject.name = "new task" }
 
       it "has a unique name" do
-        expect(task).to be_valid
+        expect(subject).to be_valid
       end
     end
 
     context "when it's invalid" do
       let!(:persisted_task) { create(:task, name: "new task") }
-      let(:task) { build(:task, name: "new task") }
+      before { subject.name = "new task" }
 
       it "doesn't have a unique name" do
-        expect(task).to be_invalid
+        expect(subject).to be_invalid
       end
     end
   end
